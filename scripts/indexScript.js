d1bfc0d7b7ead1bd7a8926daf7d114cdea1512ea
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2017-12-08 16:38:51 +0800

    added parallax

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -4,6 +4,21 @@
   //initial check to make sure jquery loaded
   //alert('jquery loaded');
 
+    //add bigIcons animation classes on mouseover
+    $(".bigIcon").on('mouseover', function () {
+        $("a.fa-github.bigIcon").addClass('animated swing infinite');
+        $("a.fa-linkedin.bigIcon").addClass('animated wobble infinite');
+        $("a.fa-facebook.bigIcon").addClass('animated bounce infinite');
+        $("a.fa-twitter.bigIcon").addClass('animated jello infinite');
+      });
+      //remove bigIcons animation classes on mouseout
+      $(".bigIcon").on('mouseout', function () {
+        $("a.fa-github.bigIcon").removeClass('animated swing infinite');
+        $("a.fa-linkedin.bigIcon").removeClass('animated wobble infinite');
+        $("a.fa-facebook.bigIcon").removeClass('animated bounce infinite');
+        $("a.fa-twitter.bigIcon").removeClass('animated jello infinite');
+      });
+
   /* make card flip on click*/
   $('.flip-container').click(function() {
     $(this).toggleClass('flipped');
